758e6e21eac7f20fe60f720e3bdb535ef2383ece
Jakson <superdormanbros@gmail.com>
2023-02-20 03:02:25 -0600

    Don't add arrow buttons if RemoveDelay is 0

--- Service/HandlerService.cs
+++ Service/HandlerService.cs
@@ -23,8 +23,10 @@ namespace CharacterAI_Discord_Bot.Service
 
             var botReply = await message.ReplyAsync(replyText, embed: embed).ConfigureAwait(false);
 
-            await SetArrowButtons(botReply).ConfigureAwait(false);
-            _ = RemoveButtons(botReply, delay: BotConfig.RemoveDelay);
+			// Skip adding buttons if delay is 0
+			if (BotConfig.RemoveDelay > 0)
+				await SetArrowButtons(botReply).ConfigureAwait(false);
+				_ = RemoveButtons(botReply, delay: BotConfig.RemoveDelay);
 
             return botReply!.Id;
         }
